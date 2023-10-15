c9fa50f20f1cd648dd5b8b3cc027d516ec1d9cda
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2023-10-15 22:58:57 +0700

    add config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraduateModule } from './graduate/graduate.module'
@@ -7,7 +8,15 @@ import { PrismaService } from './prisma/prisma.service'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-	imports: [GraduateModule, LineApiModule, PrismaModule],
+	imports: [
+		GraduateModule,
+		LineApiModule,
+		PrismaModule,
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: ['.env', '/config/.env']
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService, PrismaService],
 	exports: [PrismaService]

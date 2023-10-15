af5f7d30142068e03a55fb921a71f921bf046772
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2023-10-15 22:30:08 +0700

    remove test

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
@@ -8,10 +8,5 @@ export class AppController {
 	@Get()
 	getHello(): string {
 		return this.appService.getHello()
-	}
-
-	@Post()
-	test(@Body() body: any): void {
-		console.log(JSON.stringify(body))
 	}
 }

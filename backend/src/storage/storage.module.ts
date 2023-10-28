9bfcf69baae2e2ca6b88922d76c2b34faf5bd0ca
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2023-10-28 12:32:40 +0700

    add driveStorageService

--- backend/src/storage/storage.module.ts
+++ backend/src/storage/storage.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { BucketStorageService } from './bucketStorage.service'
 import { ConfigModule } from '@nestjs/config'
+import { DriveStorageService } from './driveStorage.service'
 
 @Module({
 	imports: [ConfigModule],
-	providers: [BucketStorageService],
-	exports: [BucketStorageService]
+	providers: [BucketStorageService, DriveStorageService],
+	exports: [BucketStorageService, DriveStorageService]
 })
 export class StorageModule {}
